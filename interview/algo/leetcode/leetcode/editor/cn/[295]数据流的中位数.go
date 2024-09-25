a9bf91fa135edf9e8907fdd9ad72cd68a9f20fdc
kackerx <kingvstr@hotmail.com>
2024-09-25 09:23:19 +0800

    算法和数据结构的练习

---
+++ interview/algo/leetcode/leetcode/editor/cn/[295]数据流的中位数.go
@@ -0,0 +1,29 @@
+package main
+//leetcode submit region begin(Prohibit modification and deletion)
+type MedianFinder struct {
+
+}
+
+
+func Constructor() MedianFinder {
+
+}
+
+
+func (this *MedianFinder) AddNum(num int)  {
+
+}
+
+
+func (this *MedianFinder) FindMedian() float64 {
+
+}
+
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddNum(num);
+ * param_2 := obj.FindMedian();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
