0863c31ea4ac88bc3c5032339d0a40409af69565
kackerx <kingvstr@hotmail.com>
2026-01-18 22:16:21 +0800

    some

--- test/unittest/domain/link.go
+++ test/unittest/domain/link.go
@@ -1,10 +1,32 @@
-package domain
+package main
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 type Link struct {
 	ID          int64
 	OriginalURL string
 	ShortCode   string
 	CreatedAt   time.Time
+}
+
+type User struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	us := &User{
+		Name: "kacker",
+		Age:  0,
+	}
+
+	fmt.Println(us)
+}
+
+func foo(u *User) error {
+	return errors.New(u.Name)
 }
